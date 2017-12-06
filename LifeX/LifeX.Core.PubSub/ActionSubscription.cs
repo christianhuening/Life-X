45ee215f84c5b6786e6c07dfd064be79d9581197
Niklas Voss <niklas.voss@gmail.com>
2017-12-06 11:35:30 +0100

    Notes about type GUIDs.

--- LifeX/LifeX.Core.PubSub/ActionSubscription.cs
+++ LifeX/LifeX.Core.PubSub/ActionSubscription.cs
@@ -1,6 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 
+// TODO: Use type GUID to map types to actions et cetera?
+//
+// "The purpose of Type.GUID is to get the value associated with the class using [Guid("...")]. 
+//  However, it also returns a guid when this attribute is not associated. The problem is where 
+//  it gets this. A small test shows that the guid is stable. I checked the guid of a class, 
+//  and verified that it changed when I renamed the class. When I renamed the class back, 
+//  I got the original guid again. However, since these guids appear out of thin air, they 
+//  shouldn't be trusted to be stable over time, releases, framework versions, etc."
+// "By examining the SSCLI implementation, the GUID is generated from a combination of the 
+//  full class name (including namespace), the full assembly name (including version 
+//  or [assembly: ComCompatibleVersion] if present, public key token etc.), and a generic "namespace" 
+//  signature for all CLR-generated GUIDs.
+//  The generation itself is deterministic (it uses an MD5 hash as per the IETF draft document) so the 
+//  GUID is stable across compilations and runs.
+//  (This is true for the SSCLI implementation and not guaranteed to be true for the actual CLR, past, present or future.)
+
 namespace LifeX.API
 {
     public class ActionSubscription<TAction> where TAction : Action
