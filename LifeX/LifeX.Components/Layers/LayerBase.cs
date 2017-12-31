3fac2a69464e5aff28675e62df544856eab5a467
Christian Hüning <christian.huening@haw-hamburg.de>
2017-12-31 17:04:57 +0100

    re-organization, adjusted namespaces, creation directories, refactored according to new layout and architecture

---
+++ LifeX/LifeX.Components/Layers/LayerBase.cs
@@ -0,0 +1,13 @@
+﻿using LifeX.API.Layer;
+using Orleans;
+using Orleans.Concurrency;
+
+namespace LifeX.Components.Layers
+{
+    [Reentrant]
+    public class LayerBase<T> : Grain<T>, ILayerBase 
+        where T : LayerState, new()
+    {
+
+    }
+}
